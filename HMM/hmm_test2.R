c89f907da623f83ba99480a1d9b8a7b1f03b1150
cocojumbo77 <jianboxue@me.com>
2015-09-16 08:45:00 +0800

    small changes

--- HMM/hmm_test2.R
+++ HMM/hmm_test2.R
@@ -21,61 +21,6 @@ ret_benchmark_5d <- na.omit(benchmark / lag(benchmark, 5) - 1)
 ret_benchmark_10d <- na.omit(benchmark / lag(benchmark, 10) - 1)
 
 
-############ 上海指数
-ret_target <- na.omit(cbind(ret_benchmark[, 1], lag(ret_benchmark[, 1], 1), 
-                            lag(ret_benchmark[, 1], 2), ret_benchmark_5d[, 1],lag(ret_benchmark_5d[,1], 1)))
-periods <- c(500, 750, 1000, 1200, 1500, 1750, 2000)
-
-n <- nrow(ret_target);
-n_start <- 3000;
-ret_strategy <- ret_target[,1] * 0;
-for (j in n_start:(n+1))
-{
-  max_sharpe <- list();
-  max_sharpe_regime <- list();
-  predicted_regime <- list();
-  max_sharpe_periods <- list();
-  for (i in 1:length(periods)) {
-    print(paste("Trying period ", periods[i]))
-    training_set = ret_target[(n_start-periods[i]):n_start]
-    gmm <- gmm_training(data_training = training_set);
-    if (gmm$J < 2) next;
-    if (gmm$J >= 5) next;
-    
-    hmm_output <- hmm_training(gmm, data_training = training_set);
-    max_sharpe[[i]] <- hmm_output$sharpe_ratio[hmm_output$sharpe_ratio_max_regime];
-    max_sharpe_regime[[i]] <- hmm_output$sharpe_ratio_max_regime
-    predicted_regime[[i]] <- hmm_output$hmm_predict_regime
-    max_sharpe_periods[[i]] <- i;
-  }
-  
-  max_sharpe <- do.call(rbind, max_sharpe)
-  max_sharpe_regime <- do.call(rbind, max_sharpe_regime)
-  max_sharpe_periods <- do.call(rbind, max_sharpe_periods)
-  predicted_regime <- do.call(rbind, predicted_regime)
-  
-  selected_max_sharpe <-  sort(max_sharpe, index.return=TRUE, decreasing=TRUE)
-  selected_max_sharpe_index = head(selected_max_sharpe$ix, 1)
-  selected_max_sharpe_regime <- max_sharpe_regime[selected_max_sharpe_index]
-  selected_predicted_regime <- predicted_regime[selected_max_sharpe_index]
-  print("yes")
-  yesno <- (selected_predicted_regime == selected_max_sharpe_regime)
-  ret_strategy[j] <- ret_target[j, 1] * as.integer(yesno)
-  
-  print(paste("--------------", as.character(index(ret_target[j]))))
-  print(paste("best period =", periods[max_sharpe_periods[selected_max_sharpe_index]]))
-  print(paste("best regime =", selected_max_sharpe_regime, ": predict regime = ", selected_predicted_regime))
-  print(paste("prev ret = ", ret_target[j-1, 1], ": current ret= ", ret_target[j, 1]))
-  print(paste("selected ret =", ret_strategy[j], ": cumu ret =", sum(ret_strategy)))
-  
-  if (j >= (n_start+10)){
-    ret_c <- cbind(ret_strategy[(n_start):(j+1)], ret_target[(n_start):(j+1), 1])
-    charts.PerformanceSummary(ret_c)
-    
-  }
-  print("--------------------------------------------")
-  
-}
 
 
 ############################################################################
@@ -160,7 +105,62 @@ for (j in n_start:(n-1))
 
 
 #############################################################################
+############ 上海指数
+ret_target <- na.omit(cbind(ret_benchmark[, 1], lag(ret_benchmark[, 1], 1), 
+                            lag(ret_benchmark[, 1], 2), ret_benchmark_5d[, 1],lag(ret_benchmark_5d[,1], 1)))
+periods <- c(500, 750, 1000, 1200, 1500, 1750, 2000)
 
+n <- nrow(ret_target);
+n_start <- 3000;
+ret_strategy <- ret_target[,1] * 0;
+for (j in n_start:(n+1))
+{
+  max_sharpe <- list();
+  max_sharpe_regime <- list();
+  predicted_regime <- list();
+  max_sharpe_periods <- list();
+  for (i in 1:length(periods)) {
+    print(paste("Trying period ", periods[i]))
+    training_set = ret_target[(n_start-periods[i]):n_start]
+    gmm <- gmm_training(data_training = training_set);
+    if (gmm$J < 2) next;
+    if (gmm$J >= 5) next;
+    
+    hmm_output <- hmm_training(gmm, data_training = training_set);
+    max_sharpe[[i]] <- hmm_output$sharpe_ratio[hmm_output$sharpe_ratio_max_regime];
+    max_sharpe_regime[[i]] <- hmm_output$sharpe_ratio_max_regime
+    predicted_regime[[i]] <- hmm_output$hmm_predict_regime
+    max_sharpe_periods[[i]] <- i;
+  }
+  
+  max_sharpe <- do.call(rbind, max_sharpe)
+  max_sharpe_regime <- do.call(rbind, max_sharpe_regime)
+  max_sharpe_periods <- do.call(rbind, max_sharpe_periods)
+  predicted_regime <- do.call(rbind, predicted_regime)
+  
+  selected_max_sharpe <-  sort(max_sharpe, index.return=TRUE, decreasing=TRUE)
+  selected_max_sharpe_index = head(selected_max_sharpe$ix, 1)
+  selected_max_sharpe_regime <- max_sharpe_regime[selected_max_sharpe_index]
+  selected_predicted_regime <- predicted_regime[selected_max_sharpe_index]
+  print("yes")
+  yesno <- (selected_predicted_regime == selected_max_sharpe_regime)
+  ret_strategy[j] <- ret_target[j, 1] * as.integer(yesno)
+  
+  print(paste("--------------", as.character(index(ret_target[j]))))
+  print(paste("best period =", periods[max_sharpe_periods[selected_max_sharpe_index]]))
+  print(paste("best regime =", selected_max_sharpe_regime, ": predict regime = ", selected_predicted_regime))
+  print(paste("prev ret = ", ret_target[j-1, 1], ": current ret= ", ret_target[j, 1]))
+  print(paste("selected ret =", ret_strategy[j], ": cumu ret =", sum(ret_strategy)))
+  
+  if (j >= (n_start+10)){
+    ret_c <- cbind(ret_strategy[(n_start):(j+1)], ret_target[(n_start):(j+1), 1])
+    charts.PerformanceSummary(ret_c)
+    
+  }
+  print("--------------------------------------------")
+  
+}
+##############################################
 
 output <- list();
 for (i in 1:length(periods)) {
